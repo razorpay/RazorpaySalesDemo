1a3dd1437caa2a121abf7d9c0ab5356d7ace84bf
Nash Vail <nishantve1@gmail.com>
2016-12-28 17:43:56 +0530

    Styled merchant detail view

--- src/Components/MerchantDetail.js
+++ src/Components/MerchantDetail.js
@@ -11,25 +11,38 @@ class MerchantDetail extends Component {
 
     return (
       <div className={mDetailClasses}>
-        <input
+        <div className="merchantInfo">
+          <div className="merchantLogo"></div>
+          <h1>{this.props.data.name}</h1>
+        </div>
+        <div className="inputWrap">
+          <label>Merchant Name</label>
+          <input
           className="input"
           type="text"
           placeholder={this.props.data.name}
           onChange={this.props.onNameChange}/>
+        </div>
 
-        <input
-          className="input"
-          type="text"
-          placeholder={this.props.data.desc}
-          onChange={this.props.onDescChange}
-        />
+        <div className="inputWrap">
+          <label>Payment Description</label>
+          <input
+            className="input"
+            type="text"
+            placeholder={this.props.data.desc}
+            onChange={this.props.onDescChange}
+          />
+        </div>
 
-        <input
-          className="input"
-          type="text"
-          placeholder={this.props.data.amount}
-          onChange={this.props.onAmountChange}
-        />
+        <div className="inputWrap">
+          <label>Amount</label>
+          <input
+            className="input"
+            type="text"
+            placeholder={this.props.data.amount}
+            onChange={this.props.onAmountChange}
+          />
+        </div>
       </div>
     );
   }
